363e9eabe7366b627ce56a975c2f85c7cdb3a231
Mariz Youssef <mariz.youssef7@gamil.com>
2025-12-19 20:14:52 +0200

    update User schema to include role

--- src/DB/Models/UserModel.js
+++ src/DB/Models/UserModel.js
@@ -1,20 +1,15 @@
 import mongoose from "mongoose";
 import { SYSTEM_ROLE } from "../../Constants/constants.js";
 
-const UserSchema = new mongoose.Schema(
-  {
-    fullName: String,
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-    role: { type: String, enum: Object.values(SYSTEM_ROLE)},
-    phone: String,
-    avatarUrl: String,
-    isActive: { type: Boolean, default: true },
-    lastLoginAt: Date,
-    refreshToken: { type: String, select: false},
-  },
-  { timestamps: true }
-);
+const UserSchema = new mongoose.Schema({
+  userName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, select: false },
+  role: { type: String, enum: ["user", "company"], required: true },
+  refreshToken: { type: String, select: false },
+  avatar: String,
+});
+
 
 const UserModel = mongoose.models.User || mongoose.model("User", UserSchema);
 export default UserModel;
