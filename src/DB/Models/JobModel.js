c29c7bfefe8e662c477b6b7a4cc5b7dd5e62cb41
000Shehab000 <shhab200025@gmail.com>
2025-12-19 23:12:13 +0200

    Refactor JobModel schema for improved readability and consistency

--- src/DB/Models/JobModel.js
+++ src/DB/Models/JobModel.js
@@ -1,73 +1,83 @@
 // models/Job.js
-import mongoose from "mongoose";
-import { JOB_STATUS, QUESTION_TYPE, jobCategoryValues, jobEmploymentTypeValues, JOB_BENEFITS } from "../../Constants/constants.js";
+import mongoose from 'mongoose'
+import CompanyModel from './CompanyModel.js'
+import {
+  QUESTION_TYPE,
+  jobCategoryValues,
+  jobEmploymentTypeValues,
+  JOB_BENEFITS,
+} from '../../Constants/constants.js'
 
 const JobSchema = new mongoose.Schema(
   {
-    companyId: { type: mongoose.Schema.Types.ObjectId, ref: "Company", required: true },
+    companyId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Company',
+      required: true,
+    },
     title: { type: String, required: true },
 
     employmentTypes: {
       type: [String],
       required: true,
-      enum: jobEmploymentTypeValues
+      enum: jobEmploymentTypeValues,
     },
 
     salaryMin: {
       type: Number,
-      required: true
+      required: true,
     },
     salaryMax: {
       type: Number,
-      required: true
+      required: true,
     },
     salaryCurrency: {
       type: String,
-      required: true
+      required: true,
     },
 
     postDate: { type: Date, default: Date.now },
     dueDate: { type: Date },
     isLive: {
       type: Boolean,
-      default: true
+      default: true,
     },
 
     description: {
       type: String,
-      required: true
+      required: true,
     },
     responsibilities: {
       type: [String],
-      required: true
+      required: true,
     },
     whoYouAre: {
       type: [String],
-      required: true
+      required: true,
     },
     niceToHaves: {
       type: [String],
-      required: true
+      required: true,
     },
 
     categories: {
       type: [String],
       required: true,
-      enum: jobCategoryValues
+      enum: jobCategoryValues,
     },
 
     skillsIds: {
       type: [
         {
           type: mongoose.Schema.Types.ObjectId,
-          ref: "Skill"
-        }
+          ref: 'Skill',
+        },
       ],
-      required: true
+      required: true,
     },
 
-    // questions embedded 
-    // Mongoose adds an _id to these objects automatically. 
+    // questions embedded
+    // Mongoose adds an _id to these objects automatically.
     // We will use that _id to map answers to questions safely in 'Job Application Schema'.
     questions: [
       {
@@ -77,16 +87,16 @@ const JobSchema = new mongoose.Schema(
           enum: Object.values(QUESTION_TYPE),
           default: QUESTION_TYPE.TEXT,
         },
-      }
+      },
     ],
 
-    benefits: {
-      type: [JOB_BENEFITS],
-      default: []
-    }
+    // benefits: {
+    //   type: JOB_BENEFITS,
+    //   default: [],
+    // },
   },
   { timestamps: true }
-);
+)
 
-const JobModel = mongoose.models.Job || mongoose.model("Job", JobSchema);
-export default JobModel;
+const JobModel = mongoose.models.Job || mongoose.model('Job', JobSchema)
+export default JobModel
