60b8c86daa8884e49a793ccbe7d4f417d3f8231b
000Shehab000 <shhab200025@gmail.com>
2025-12-20 22:43:31 +0200

    Fix AuthenticationMiddleware

--- src/Middlewares/AuthenticationMiddleware.js
+++ src/Middlewares/AuthenticationMiddleware.js
@@ -1,37 +1,32 @@
-import  jwt  from 'jsonwebtoken';
-import UserModel from '../DB/Models/UserModel.js';
+import jwt from 'jsonwebtoken'
+import UserModel from '../DB/Models/UserModel.js'
 // import { BlacklistToken_Model } from './../DB/models/BlackList_token_model.js';
-import UserModel from '../DB/Models/UserModel.js';
-
-
-
 
 export const AuthenticationMiddleware = () => {
   return async (req, res, next) => {
     try {
-      const { access_token } = req.headers;
+      const { access_token } = req.headers
 
       if (!access_token) {
-        return res.status(401).json({ message: "please login first" });
+        return res.status(401).json({ message: 'please login first' })
       }
 
       // decode the data
       const decoding_access_token = jwt.verify(
         access_token,
         process.env.JWT_ACCESS_TOKEN_SECRET_KEY
-      );
-
+      )
 
       // find the data
       const user = await UserModel.findById(
         decoding_access_token.id,
-        "-password -__v"
-      );
+        '-password -__v'
+      )
       if (!user) {
-        return res.status(404).json({ message: "this user is not found" });
+        return res.status(404).json({ message: 'this user is not found' })
       }
 
-      console.log(user._doc);
+      console.log(user._doc)
 
       req.login_user = {
         ...user._doc,
@@ -39,62 +34,69 @@ export const AuthenticationMiddleware = () => {
           token_id: decoding_access_token.jti,
           expiration_data: decoding_access_token.exp,
         },
-      };
+      }
 
-      next();
+      next()
     } catch (error) {
       console.log(
-        "internal authentication middleware error  ==========>",
+        'internal authentication middleware error  ==========>',
         error
-      );
+      )
+      res.status(500).json({
+        message: 'internal authentication middleware error====>',
+        error,
+      })
+    }
+  }
+}
+
+//             ===================   still under test for black list  ========================
+export const AuthorizationMiddleware = (allow_role) => {
+  return async (req, res, next) => {
+    try {
+      const { access_token } = req.headers
+
+      // decode the data
+      const decoding_access_token = jwt.verify(
+        access_token,
+        process.env.JWT_ACCESS_TOKEN_SECRET_KEY
+      )
+
+      // check if in black list
+      const if_black_list = await BlacklistToken_Model.findOne({
+        where: { token_id: decoding_access_token.jti },
+      })
+      if (if_black_list) {
+        return res
+          .status(401)
+          .json({ message: 'this token is expired please login again' })
+      }
+
+      // find the data
+      const User = await UserModel.findByPk(decoding_access_token.id)
+      if (!User) {
+        return res.status(404).json({ message: 'this user is not found' })
+      }
+
+      // console.log( User  , "ddddddddddddd" );
+      req.login_user = {
+        ...User._doc,
+        token: {
+          token_id: decoding_access_token.jti,
+          expiration_data: decoding_access_token.exp,
+        },
+      }
+      // console.log( req.login_user );
+
+      next()
+    } catch (error) {
+      console.log('internal authorization middleware  error=====>', error)
       res
         .status(500)
         .json({
-          message: "internal authentication middleware error====>",
+          message: 'internal authorization middleware  error====>',
           error,
-        });
+        })
     }
-  };
-};
-
-
-
-
-
-//             ===================   still under test for black list  ========================
-export const AuthorizationMiddleware = (allow_role) =>{
-    return async ( req , res , next ) =>{
- 
-        try {
-            const { access_token } = req.headers
-
-            // decode the data
-            const decoding_access_token = jwt.verify(access_token , process.env.JWT_ACCESS_TOKEN_SECRET_KEY)
-
-            // check if in black list
-            const if_black_list = await BlacklistToken_Model.findOne({ where : { token_id:decoding_access_token.jti } })
-            if ( if_black_list) {return res.status(401).json({ message :"this token is expired please login again"  }) }
-
-            // find the data
-            const User = await UserModel.findByPk(decoding_access_token.id  )
-            if (!User) { return res.status(404).json({ message :"this user is not found" }) }
-            
-            // console.log( User  , "ddddddddddddd" );
-            req.login_user = { ...User._doc , token:{  token_id:decoding_access_token.jti , expiration_data:decoding_access_token.exp } }
-            // console.log( req.login_user );
-            
-            
-            next()
-        } catch (error) {
-            console.log(  "internal authorization middleware  error=====>" , error );
-            res.status(500).json({ message : "internal authorization middleware  error====>" , error })
-        }
-
-    }
+  }
 }
-
-
-
-
-
-
