68721cf2243623432f3006e044a30759d13071af
000Shehab000 <shhab200025@gmail.com>
2025-12-25 22:11:35 +0200

    Refactor getJobApplicationService to accept only applicationId and update controller accordingly

--- src/Modules/jobApplications/services/jobApplications.service.js
+++ src/Modules/jobApplications/services/jobApplications.service.js
@@ -9,11 +9,8 @@ export const getAllJobApplicationsService = async (filter = {}) => {
   return jobApplications
 }
 
-export const getJobApplicationService = async (jobId, applicationId) => {
-  const jobApplication = await JobApplicationModel.findOne({
-    _id: applicationId,
-    jobId,
-  })
+export const getJobApplicationService = async (applicationId) => {
+  const jobApplication = await JobApplicationModel.findById(applicationId)
   if (!jobApplication) {
     throw new ApiError(404, 'jobapplication notfound')
   }
