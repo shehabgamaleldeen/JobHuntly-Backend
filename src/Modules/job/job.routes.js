42203982708cee94e023892a7862588a7fa6ff8b
000Shehab000 <shhab200025@gmail.com>
2025-12-26 23:51:48 +0200

    Refactor job routes to include optional authentication for getting job by ID

--- src/Modules/job/job.routes.js
+++ src/Modules/job/job.routes.js
@@ -1,10 +1,17 @@
 import { Router } from 'express'
 import * as jobController from '../job/controllers/job.controller.js'
-import { AuthenticationMiddleware } from '../../Middlewares/AuthenticationMiddleware.js'
+import {
+  AuthenticationMiddleware,
+  OptionalAuthenticationMiddleware,
+} from '../../Middlewares/AuthenticationMiddleware.js'
 const JobRouter = Router()
 
 JobRouter.get('/', jobController.getAllJobs)
-JobRouter.get('/:id', jobController.getJobById)
+JobRouter.get(
+  '/:id',
+  OptionalAuthenticationMiddleware(),
+  jobController.getJobById
+)
 
 // protected route need auth and a user object in req body
 JobRouter.post(
