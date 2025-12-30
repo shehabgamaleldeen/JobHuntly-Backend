72f5829eb86e205aec245443e5e702facd9ad169
MoazRyhan <moazryhan@gmail.com>
2025-12-30 11:53:48 +0200

    put the cluster connection string

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -6,8 +6,8 @@ const DataBase =  async  () =>{
 
     try {
         
-        const connection = await mongoose.connect( `${ process.env.DB_URI }`) // this is for local 
-        // const connection = await mongoose.connect( `${ process.env.DB_MONGO_ATLAS }`) // this is for production 
+        // const connection = await mongoose.connect( `${ process.env.DB_URI }`) // this is for local 
+        const connection = await mongoose.connect( `${ process.env.DB_MONGO_ATLAS}` ,  {dbName: "jobhuntly"} ) // this is for production 
 
         if (connection) {
             console.log( "database is working" );
